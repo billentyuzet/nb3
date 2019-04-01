4014e8e21568ce62fa24ffcfc51986bd38639e3b
netbeansZsoma <pzsferrari@freemail.hu>
2019-04-01 15:44:03 +0200

    uj fajl

---
+++ kozos_projekt_2/src/kozos_projekt_2/Kozos_projekt_2.java
@@ -0,0 +1,16 @@
+
+package kozos_projekt_2;
+
+public class Kozos_projekt_2 {
+
+   
+         
+    public static void main(String[] args) 
+    {
+             
+         System.out.println("Barack");
+      
+         
+    }
+    
+}
